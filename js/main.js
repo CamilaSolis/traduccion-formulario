6e38079d986c989b51e2b4beabe8770afb74b3a8
CAMILA <ivonnemunoz@new-host.home>
2016-10-24 18:19:46 -0300

    Traduccion formulario

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function traductor(){
+
+document.getElementById('form-signin-heading').innerHTML="Por favor inicia sesión";
+document.getElementById('inputEmail').placeholder="Correo Electronico";
+document.getElementById('inputPassword').placeholder="Contraseña";
+document.querySelector('.checkbox span').innerHTML="Recordar datos";
+document.querySelector('button').innerHTML="Iniciar sesión"
+}
